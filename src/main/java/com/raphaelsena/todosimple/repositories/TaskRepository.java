9f533036ffe6cb95d516e6cf0cba13bea0702969
raphael sena <rsenares1@gmail.com>
2024-04-23 21:48:57 -0300

    Create User and Task repositories

---
+++ src/main/java/com/raphaelsena/todosimple/repositories/TaskRepository.java
@@ -0,0 +1,21 @@
+package com.raphaelsena.todosimple.repositories;
+
+import com.raphaelsena.todosimple.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+     List<Task> findByUser_Id(Long id);
+
+//     @Query(value = "SELECT t FROM Task t WHERE t.user.id = :id")
+//     List<Task> findByUser_Id(@Param("id") Long id);
+
+//    @Query(value = "SELECT * FROM task t WHERE t.user_id = :id", nativeQuery = true)
+//    List<Task> findByUser_Id(@Param("id") Long id);
+}
---
+++ src/main/java/com/raphaelsena/todosimple/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.raphaelsena.todosimple.repositories;
+
+import com.raphaelsena.todosimple.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
