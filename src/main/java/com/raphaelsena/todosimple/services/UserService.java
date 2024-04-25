069b55d4bc3d00825f6a0fa44c4766e796a0a7b3
raphael sena <rsenares1@gmail.com>
2024-04-25 00:00:52 -0300

    Create TaskService

--- src/main/java/com/raphaelsena/todosimple/services/UserService.java
+++ src/main/java/com/raphaelsena/todosimple/services/UserService.java
@@ -1,15 +1,14 @@
 package com.raphaelsena.todosimple.services;
 
-import com.raphaelsena.todosimple.models.User;
-import com.raphaelsena.todosimple.repositories.TaskRepository;
-import com.raphaelsena.todosimple.repositories.UserRepository;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
-import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
+import com.raphaelsena.todosimple.repositories.UserRepository;
+import com.raphaelsena.todosimple.models.User;
+import com.raphaelsena.todosimple.repositories.TaskRepository;
 
 @Service
 public class UserService {
@@ -23,13 +22,12 @@ public class UserService {
     public User findById(Long id) {
         Optional<User> user = this.userRepository.findById(id);
         return user.orElseThrow(() -> new RuntimeException(
-                "Usuário não encontrado! Id: " + id + ", Tipo: " + User.class.getName()
-        ));
+                "Usuário não encontrado! Id: " + id + ", Tipo: " + User.class.getName()));
     }
 
     @Transactional
     public User create(User obj) {
-        obj.setId(id: null);
+        obj.setId(null);
         obj = this.userRepository.save(obj);
         this.taskRepository.saveAll(obj.getTasks());
         return obj;
@@ -42,15 +40,13 @@ public class UserService {
         return this.userRepository.save(newObj);
     }
 
-    @Transactional
     public void delete(Long id) {
         findById(id);
         try {
             this.userRepository.deleteById(id);
         } catch (Exception e) {
-            throw new RuntimeException(
-                    "Não é possível excluir, pois há entidades relacionadas!"
-            );
+            throw new RuntimeException("Não é possível excluir pois há entidades relacionadas!");
         }
     }
+
 }
