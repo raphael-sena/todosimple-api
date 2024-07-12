1368c0266602215cece7e158d80e51185ca2ac36
raphael sena <rsenares1@gmail.com>
2024-07-12 15:03:19 -0300

    refactor: frontend refactor

--- view/scripts/script.js
+++ view/scripts/script.js
@@ -1,24 +1,19 @@
-const url = "http://localhost:8080/task/user/1";
+const tasksEndpoint = "http://localhost:8080/task/user";
 
 function hideLoader() {
     document.getElementById("loading").style.display = "none";
 }
-
 function show(tasks) {
     let tab = `<thead>
             <th scope="col">#</th>
             <th scope="col">Description</th>
-            <th scope="col">Username</th>
-            <th scope="col">User Id</th>
-            </thead>`;
+        </thead>`;
 
     for (let task of tasks) {
         tab += `
             <tr>
                 <td scope="row">${task.id}</td>
                 <td>${task.description}</td>
-                <td>${task.user.username}</td>
-                <td>${task.user.id}</td>
             </tr>
         `;
     }
@@ -26,15 +21,24 @@ function show(tasks) {
     document.getElementById("tasks").innerHTML = tab;
 }
 
-async function getAPI(url) {
-    const response = await fetch(url, { method: "GET" });
+async function getTasks() {
+    let key = "Authorization";
+    const response = await fetch(tasksEndpoint, {
+        method: "GET",
+        headers: new Headers({
+            Authorization: localStorage.getItem(key),
+        }),
+    });
 
     var data = await response.json();
     console.log(data);
-
-    if(response)
-        hideLoader();
+    if (response) hideLoader();
     show(data);
 }
 
-getAPI(url);
+document.addEventListener("DOMContentLoaded", function (event) {
+    if (!localStorage.getItem("Authorization"))
+        window.location = "/view/login.html";
+});
+
+getTasks();
